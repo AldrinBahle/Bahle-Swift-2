bdee9f71e760e086d5e9bd12af222517c1c9f46a
aldrinbgama <aldrin.gama@umuzi.org>
2020-05-28 13:12:04 +0200

    Initial Commit

---
+++ myApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myApp1
+//
+//  Created by Aldrin Bahle Gama on 2020/05/28.
+//  Copyright © 2020 Aldrin Bahle Gama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
